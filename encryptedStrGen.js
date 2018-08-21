36e914de70e64ad3f9f45c102d91c06086781ba9
amrendra007 <amrendrapcs07@gmail.com>
2018-08-21 20:55:01 +0530

    Create seprate file for generating encrypted string

---
+++ encryptedStrGen.js
@@ -0,0 +1,30 @@
+const bcrypt = require('bcrypt');
+const util = require('./util');
+
+const saltRounds = 10;
+const encrArray = [];
+
+module.exports = function () {
+    return new Promise((resolve, reject) => {
+        for (let i = 0; i < 10; i += 1) {
+            const obj = util.objCreator();
+
+            // Hashing to add secret_key
+            bcrypt.hash(JSON.stringify(obj), saltRounds)
+                .then((hash) => {
+                    obj.secret_key = hash;
+
+                    // Encrypting whole stringified obj
+                    const raw = util.encryptor(JSON.stringify(obj));
+                    encrArray.push(raw);
+                    if (encrArray.length === 10) {
+                        resolve(encrArray.join('|'));
+                    }
+                })
+                .catch((err) => {
+                    console.log(err);
+                    reject(err);
+                });
+        }
+    });
+};
