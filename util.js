f869e7628cd079ea90818c7ae0da7f497598175a
amrendra007 <amrendrapcs07@gmail.com>
2018-08-22 00:19:07 +0530

    Improve encryption logic

--- util.js
+++ util.js
@@ -1,25 +1,29 @@
-const crypto = require('crypto');
+const Crypto = require('crypto');
 require('dotenv').config();
 
-const algorithm = process.env.ALGO;
-const password = process.env.PSWD;
+// Random function Generator be
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+module.exports.random = random;
 
+// Generating random obj
 const names = ['Jack', 'Mack', 'wack', 'Rocky', 'Tuckker', 'Bang', 'sack', 'zeem', 'jhdfkjd', 'cddd'];
 const origins = ['New york', 'delhi', 'mumbai', 'chennai', 'New boston', 'LA', 'Ca', 'Sa', 'ma', 'gtsy'];
 const destinations = [' Delhi', 'Hyderabad', 'Ahmedabad', 'Chennai', 'Bangalore ', 'Kolkata', 'Surat', 'Pune', 'Jaipu', 'Lucknow'];
-
 module.exports.objCreator = function() {
     return {
-        name: names[Math.floor(Math.random() * 10)],
-        origin: origins[Math.floor(Math.random() * 10)],
-        destination: destinations[Math.floor(Math.random() * 10)],
+        name: names[random(0, 9)],
+        origin: origins[random(0, 9)],
+        destination: destinations[random(0, 9)],
     };
 };
 
-
+// Encrypting string
 module.exports.encryptor = function(text) {
-    const cipher = crypto.createCipher(algorithm, password);
-    let crypted = cipher.update(text, 'utf8', 'hex');
-    crypted += cipher.final('hex');
-    return crypted;
+    const iv = Crypto.randomBytes(16);
+    const cipher = Crypto.createCipheriv(process.env.ALGO, Buffer.from(process.env.PSWD), iv);
+    const encrypted = cipher.update(text);
+    const finalBuffer = Buffer.concat([encrypted, cipher.final()]);
+    return `${iv.toString('hex')}:${finalBuffer.toString('hex')}`;
 };
