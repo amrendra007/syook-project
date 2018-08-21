0534fb2e044f480c873146feed15b7b6d779dcef
amrendra007 <amrendrapcs07@gmail.com>
2018-08-22 02:08:50 +0530

    Remove Global Array

--- helper/encryptedStrGen.js
+++ helper/encryptedStrGen.js
@@ -2,9 +2,9 @@ const bcrypt = require('bcrypt');
 const util = require('./util');
 
 const saltRounds = 10;
-const encrArray = [];
 
 module.exports = function () {
+    const encrArray = [];
     return new Promise((resolve, reject) => {
         for (let i = 0; i < 10; i += 1) {
             const obj = util.objCreator();
@@ -18,10 +18,7 @@ module.exports = function () {
                     const raw = util.encryptor(JSON.stringify(obj));
                     encrArray.push(raw);
                     if (encrArray.length === 10) {
-                        // clearing global Array
-                        const copyArray = [...encrArray];
-                        encrArray.length = 0;
-                        resolve(copyArray.join('|'));
+                        resolve(encrArray.join('|'));
                     }
                 })
                 .catch((err) => {
