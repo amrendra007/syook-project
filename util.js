9e5699792b1bc6c14c415bdb96f3648bc6a761aa
amrendra007 <amrendrapcs07@gmail.com>
2018-08-21 19:41:05 +0530

    Improve helper function

--- util.js
+++ util.js
@@ -1,11 +1,24 @@
+const crypto = require('crypto');
+
+const algorithm = 'aes-256-ctr';
+const password = 'd6F3Efeq';
+
 const names = ['Jack', 'Mack', 'wack', 'Rocky', 'Tuckker', 'Bang', 'sack', 'zeem', 'jhdfkjd', 'cddd'];
 const origins = ['New york', 'delhi', 'mumbai', 'chennai', 'New boston', 'LA', 'Ca', 'Sa', 'ma', 'gtsy'];
 const destinations = [' Delhi', 'Hyderabad', 'Ahmedabad', 'Chennai', 'Bangalore ', 'Kolkata', 'Surat', 'Pune', 'Jaipu', 'Lucknow'];
 
-module.exports = function() {
+module.exports.objCreator = function() {
     return {
         name: names[Math.floor(Math.random() * 10)],
         origin: origins[Math.floor(Math.random() * 10)],
         destination: destinations[Math.floor(Math.random() * 10)],
     };
+};
+
+
+module.exports.encryptor = function(text) {
+    const cipher = crypto.createCipher(algorithm, password);
+    let crypted = cipher.update(text, 'utf8', 'hex');
+    crypted += cipher.final('hex');
+    return crypted;
 };
