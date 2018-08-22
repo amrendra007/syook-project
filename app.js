f18eedbdc7bff2be04da472be236bd7b92e821d8
amrendra007 <amrendrapcs07@gmail.com>
2018-08-22 15:50:44 +0530

    Set up listner server

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-const encryptedStringGen = require('./helper/encryptedStrGen');
+const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
-encryptedStringGen()
-    .then((finalString) => {
-        console.log(finalString);
-    })
-    .catch((err) => {
-        console.log(err);
+io.on('connection', (socket) => {
+    console.log('client connected');
+
+    socket.on('data', (encryptedStr) => {
+        console.log(encryptedStr);
     });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('server runnning');
+});
