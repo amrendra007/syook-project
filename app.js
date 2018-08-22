5042518ac6248124695ac2375c84e7506a255462
amrendra007 <amrendrapcs07@gmail.com>
2018-08-22 17:46:44 +0530

    Test DataIntegrity Function

--- app.js
+++ app.js
@@ -11,8 +11,10 @@ io.on('connection', (socket) => {
     console.log('client connected');
 
     socket.on('data', (encryptedStr) => {
-        dataIntegrityChecker(encryptedStr.split('|'));
-        // console.log(encryptedStr);
+        dataIntegrityChecker(encryptedStr.split('|'))
+            .then((data) => {
+                console.log(data);
+            });
     });
 
     socket.on('disconnect', () => {
