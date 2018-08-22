3107bcf8cd833b897e658da57ac9c0383677815a
amrendra007 <amrendrapcs07@gmail.com>
2018-08-22 17:45:50 +0530

    Create DataIntegrity checker function

---
+++ helper/dataIntegrityChecker.js
@@ -0,0 +1,30 @@
+const bcrypt = require('bcrypt');
+const util = require('./util');
+
+module.exports = function(arrayOfEncrStr) {
+    // console.log(arrayOfEncrStr);
+    const array = [];
+    const lengthOfEncArray = arrayOfEncrStr.length;
+
+    return new Promise((resolve, reject) => {
+        arrayOfEncrStr.map((item, index) => { // eslint-disable-line
+            const decryptedStr = util.decryptor(item);
+            const decryptedObj = util.parseJsonSafely(decryptedStr);
+
+            const { secretKey, ...originalObj } = decryptedObj;
+            bcrypt.compare(JSON.stringify(originalObj), secretKey)
+                .then((res) => {
+                    if (res === true) {
+                        array.push(originalObj);
+                    }
+                    if (lengthOfEncArray === index + 1) {
+                        resolve(array);
+                    }
+                })
+                .catch((err) => {
+                    console.log(err);
+                    reject(err);
+                });
+        });
+    });
+};
