773431ad117b0f087c878f8dafa489196f4a857a
amrendra007 <amrendrapcs07@gmail.com>
2018-08-24 01:00:12 +0530

    Emmiting encrypted str every 10s

--- emmiter.js
+++ emmiter.js
@@ -1,16 +1,17 @@
 const socket = require('socket.io-client')('http://localhost:3000');
 const encryptedStrGen = require('./helper/encryptedStrGen');
 
+// emmiting each encrypted str every 10s
 socket.on('connect', () => {
     console.log('connected to server');
 
-    // setInterval(() => {
-    encryptedStrGen()
-        .then((finalString) => {
-            socket.emit('data', finalString);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-    // }, 1000);
+    setInterval(() => {
+        encryptedStrGen()
+            .then((finalString) => {
+                socket.emit('data', finalString);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, 10000);
 });
