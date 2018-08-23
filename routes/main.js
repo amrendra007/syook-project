41dd022e8e5caaf20769cf5ef2cc384368326174
amrendra007 <amrendrapcs07@gmail.com>
2018-08-24 01:10:24 +0530

    Render Data from db to UI

---
+++ routes/main.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const Person = require('../models/person');
+
+const router = express.Router();
+
+// rendering data to ui
+router.get('/', async (req, res, next) => {
+    const personData = await Person.find({});
+    res.render('person', { personData });
+});
+
+module.exports = router;
