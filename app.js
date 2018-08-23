e9adb07ffda7ebce5d86cf7f48be2b263c08104f
amrendra007 <amrendrapcs07@gmail.com>
2018-08-23 15:44:40 +0530

    Add Data base connection

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const socketIO = require('socket.io');
 const http = require('http');
+const moment = require('moment');
+const mongoose = require('mongoose');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 const dataIntegrityChecker = require('./helper/dataIntegrityChecker');
+
+// Database conn
+mongoose.connect(`${process.env.CONN_STRING}`, { useNewUrlParser: true })
+    .then(() => {
+        console.log('connected to db');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 io.on('connection', (socket) => {
     console.log('client connected');
