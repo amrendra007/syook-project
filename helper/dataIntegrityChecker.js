603a94164bc19fac229540dc3c06cd5845b9ef60
amrendra007 <amrendrapcs07@gmail.com>
2018-08-24 01:02:23 +0530

    Create Moment Instance and make it local

--- helper/dataIntegrityChecker.js
+++ helper/dataIntegrityChecker.js
@@ -1,8 +1,13 @@
 const bcrypt = require('bcrypt');
 const moment = require('moment');
+
 const util = require('./util');
 
+/*  This function takes encrypted array of str, loop over each item, decrypt it parse to json
+    compare integrity of each obj and return array of original obj with timestamp
+*/
 module.exports = function (arrayOfEncrStr) {
+    const momentIns = moment();
     // console.log(arrayOfEncrStr);
     const array = [];
     const lengthOfEncArray = arrayOfEncrStr.length;
@@ -16,7 +21,7 @@ module.exports = function (arrayOfEncrStr) {
             bcrypt.compare(JSON.stringify(originalObj), secretKey)
                 .then((res) => {
                     if (res === true) {
-                        originalObj.timestamp = moment().format();
+                        originalObj.timestamp = momentIns.format();
                         array.push(originalObj);
                     }
                     if (lengthOfEncArray === index + 1) {
